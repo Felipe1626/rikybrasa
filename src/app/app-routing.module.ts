67364fd1ef203a6b1eaa11f97638891ebed0cc7b
Felipe Rincon <felipe.k.r.r16@gmail.com>
2023-09-06 10:52:09 -0400

    routing to admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './components/admin/admin.component';
+import { ProductsComponent } from './components/branches/components/rikybrasa/components/products/products.component';
 
 const routes: Routes = [
-    { path: '', component: AdminComponent }
+    { path: '', component: ProductsComponent },
+    { path: 'admin', component: AdminComponent }
 ]
 
 @NgModule ({
